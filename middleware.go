9bf36634a13e55c3e3d87aaa91e92c4b3647020e
flames31 <rrahulao2000@gmail.com>
2025-08-24 12:21:45 -0400

    done; can improve

--- middleware.go
+++ middleware.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/sessions"
 )
 
 func logRequest(h http.Handler) http.Handler {
@@ -12,5 +14,17 @@ func logRequest(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 		t2 := time.Since(t1)
 		log.Printf("%-10v %-10v %-10v", r.Method, r.URL, t2)
+	})
+}
+
+func isAuthenticated(h http.Handler, store *sessions.CookieStore) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "userSessions")
+		userIDStr, ok := session.Values["user_id"].(string)
+		if !ok || userIDStr == "" {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		h.ServeHTTP(w, r)
 	})
 }
--- routes.go
+++ routes.go
@@ -11,11 +11,11 @@ import (
 func addRoutes(mux *mux.Router, tmpl *template.Template, db *sql.DB, store *sessions.CookieStore) {
 
 	mux.Handle("/login", handleLogin(tmpl, db, store))
-	mux.Handle("/blogs", handleGetBlogs(tmpl, db)).Methods("GET")
-	mux.Handle("/blogs", handlePostBlog(tmpl, db, store)).Methods("POST")
+	mux.Handle("/blogs", isAuthenticated(handleGetBlogs(tmpl, db), store)).Methods("GET")
+	mux.Handle("/blogs", isAuthenticated(handlePostBlog(tmpl, db, store), store)).Methods("POST")
 	mux.Handle("/", handleIndex())
-	mux.Handle("/blogs/new", handleCreateBlog(tmpl, db))
-	mux.Handle("/myblogs", handleMyBlogs(tmpl, db, store)).Methods("GET")
-	mux.Handle("/blogs/edit/{id}", handleEditBlog(tmpl, db, store))
-	mux.Handle("/blogs/delete/{id}", handleDeleteBlog(tmpl, db, store))
+	mux.Handle("/blogs/new", isAuthenticated(handleCreateBlog(tmpl, db), store))
+	mux.Handle("/myblogs", isAuthenticated(handleMyBlogs(tmpl, db, store), store)).Methods("GET")
+	mux.Handle("/blogs/edit/{id}", isAuthenticated(handleEditBlog(tmpl, db, store), store))
+	mux.Handle("/blogs/delete/{id}", isAuthenticated(handleDeleteBlog(tmpl, db, store), store))
 }
